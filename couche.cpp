ce82af89d47824ab47c4c35d047c70009343ed34
ShadossOw <118916265+ShadossOw@users.noreply.github.com>
2026-01-12 17:49:01 -0500

    Update couche.cpp non teste

--- couche.cpp
+++ couche.cpp
@@ -8,11 +8,13 @@
  * Ce fichier fait partie de la distribution de Graphicus.
 ********/
 
-#include "couche.h"
+#include "couche.h"  
+
 
 // A completer
 Couche::Couche()
 {
+    
     State = INITIALIZED;
     totalArea = 0;
 }
@@ -32,36 +34,52 @@ Couche::~Couche()
 //}
 
 // A completer
-bool Couche::AddShape()
+bool Couche::AddShape(forme* Shape)
 {
-    if (State == INACTIVE)
+    if(State == ACTIVE)
     {
-        return false;
+        if (Shape == nullptr)
+        {
+            return false;
+        }
+        VectorCouche.addElement(Shape);
+        return true;
     }
-    else if (State == ACTIVE)
+    else
     {
+        return 0;
+    }
+
         //Ajouter une forme au vecteur et a l'affichage
         //Check if operation is successful
         //return true if successful
-    }
     //Elle permet l’ajout d’une forme à la suite des autres formes déjà présente. 
     //La forme à ajouter est spécifiée par son pointeur. 
     //Une valeur de retour booléenne indique si l’opération est une réussite ou un échec.
 }
 
 // A completer
-bool Couche::RemoveShape()
+int Couche::RemoveShape(int index)
 {
-    if (State == INACTIVE)
+    if(State == ACTIVE)
     {
-        return false;
+        if (index >= VectorCouche.getSize())
+        {
+            return nullptr;
+        }
+        int ptrForme = VectorCouche.getData(index);
+        VectorCouche.removeElement(index);
+        return ptrForme;
     }
-    else if (State == ACTIVE)
+    else
     {
+        return 0;
+    }
+
         //Enlever une forme du vecteur et de l'affichage
         //Check if operation is successful
         //return true if successful
-    }
+    
     //Elle permet le retrait d’une forme. La forme à retirer est spécifiée par son index dans le vecteur.
     //Si l’opération est une réussite, alors la valeur de retour est le pointeur sur la forme en question. 
     //Si l’opération est un échec, comme avec un index invalide, un pointeur nul est retourné. 
@@ -69,45 +87,69 @@ bool Couche::RemoveShape()
 }
 
 // A completer
-//int Couche::GetShape(char VecIndex) const
-//{
+Forme* Couche::GetShape(int index)
+{
+    if(State == ACTIVE)
+    {
+        if (index >= VectorCouche.getSize())
+        {
+            return nullptr;
+        }
+        return VectorCouche.getData(index);
+    }
+    else
+    {
+        return 0;
+    }
+
     //Elle permet d’obtenir une forme stockée. La forme à obtenir est spécifiée par son index dans le vecteur.
     //Si l’opération est une réussite, alors la valeur de retour est le pointeur sur la forme en question. 
     //Si l’opération est un échec, comme avec un index invalide, un pointeur nul est retourné. 
     //La forme n’est pas retirée du vecteur et elle n’est pas détruite.
-//}
+}
 
 // A completer
-float Couche::GetTotalArea() const
+double Couche::GetTotalArea() 
 {
-    if (State == INACTIVE)
+    if(State == ACTIVE)
     {
-        return false;
+        int TotalArea = 0;
+        for (int i = 0; i < VectorCouche.getSize(); ++i)
+        {
+            TotalArea = TotalArea + VectorCouche.getData(i)->getArea();
+        }
+        return TotalArea;
     }
-    else if (State == ACTIVE)
+    else
     {
-        //Calculer l’aire totale de la couche
-        //Check if operation is successful
-        //return true if successful
+        return 0;
     }
-    //Elle offre la possibilité d’obtenir l’aire totale.
-    //L’aire totale de la couche est définie comme la somme des aires des formes qui la compose. 
-    //La valeur de retour, l’aire totale, est un nombre réel.
+    
+    
 }
 
 // A completer
 bool Couche::MoveLayer(int deltaX, int deltaY)
 {
-    if (State == INACTIVE)
+    
+    if(State == ACTIVE)
     {
-        return false;
+        for (int i = 0; i < VectorCouche.getSize(); ++i)
+        {
+            int newX = VectorCouche.getData(i)->getX() + deltaX;
+            int newY = VectorCouche.getData(i)->getY() + deltaY;
+            VectorCouche.getData(i)->setBottomLeft(newX, newY);
+        }
+        return 1;
     }
-    else if (State == ACTIVE)
+    else
     {
+        return 0;
+    }
         //Translater toutes les formes de la couche
         //Check if operation is successful
         //return true if successful
-    }
+    
     //Elle offre la possibilité de translater. 
     //La translation d’une couche est réalisée en faisant la translation de toutes les formes qui la compose. 
     //Les valeurs de translation horizontale et verticale, des entiers, sont passées en paramètres. 
@@ -117,28 +159,15 @@ bool Couche::MoveLayer(int deltaX, int deltaY)
 // A completer
 bool Couche::ResetLayer()
 {
-    if (State == INACTIVE)
-    {
-        return false;
-    }
-    else if (State == ACTIVE)
+    if(State != INITIALIZED)
     {
         State = INITIALIZED;
-        totalArea = 0;
-        //Vider le vecteur de toutes les formes
-
-        unsigned char verifyState = State;
-        float verifyArea = totalArea;
-        //Valeur de verification pour le vecteur a ajouter
-
-        if (verifyState == INITIALIZED && verifyArea == 0 /*&& vecteur de formes est vide*/)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        VectorCouche.deleteData();
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
     //Elle offre la possibilité de réinitialiser. 
     //L’état de la couche est donc ramené à son état initial, comme lors de sa création. 
@@ -147,7 +176,7 @@ bool Couche::ResetLayer()
     
 }
 
-bool Couche::SetState(unsigned char newState)
+bool Couche::SetState(int newState)
 {
     State = newState;
     unsigned char verifyState = State;
@@ -162,10 +191,31 @@ bool Couche::SetState(unsigned char newState)
 }
 
 // A completer
-//Couche::DisplayLayer();
-//{
+void Couche::DisplayLayer(std::ostream& out)
+{
+     out << "[Couche ";
+
+    if (State == INITIALIZED)
+        out << "INITIALIZED] ";
+    else if (State == ACTIVE)
+        out << "ACTIVE] ";
+    else
+        out << "INACTIVE] ";
+
+    for (int i = 0; i < VectorCouche.getSize(); ++i)
+    {
+        if (VectorCouche.getData(i) != nullptr)
+        {
+            out << *(VectorCouche.getData(i)) << " "; // Supposons que Forme a un opérateur<<
+        }
+        else
+        {
+            out << "[vide] ";
+        }
+    }
+    out << std::endl;
     //Elle permet l’affichage du contenu, donc de toutes ses formes. 
     //L’endroit à afficher (le flot) est spécifié en paramètre. 
     //La figure A.2 montre comment une couche doit être affichée. 
     //On peut d’ailleurs y observer que l’état de la couche affecte l’affichage.
-//}
+}
