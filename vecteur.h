c1859ca5018e2c7a5bb3332a7ea6019870a80a24
ShadossOw <118916265+ShadossOw@users.noreply.github.com>
2026-01-12 11:59:56 -0500

    rajout de chaque fonction (non testé)

--- vecteur.h
+++ vecteur.h
@@ -10,10 +10,9 @@ class Vector
         int capacity_;                  //Maximum amount of elements that can be stored without realocating
         T* data_;                       //Pointer to the array of elements (can be any type since we are using a template)
 
-        void reserve(int newCapacity)   //Function to reallocate memory when capacity is exceeded
+        void resize()   //Function to reallocate memory when capacity is exceeded
         {
-            if (newCapacity <= capacity_) {return;}
-                
+            int newCapacity = (capacity_ == 0) ? 1 : capacity * 2; // au cas ou capacity == 0
             T* newData = new T[newCapacity];
             for (int i = 0; i < size_; ++i)
             {
@@ -40,8 +39,10 @@ class Vector
         }
 
     public:
-        Vector();
-        ~Vector();
+        Vector() : data_(nullptr), size_(0), capacity_(1);
+        ~Vector(){
+            delete[] data_;
+        }
 
         int getSize() const
         {
@@ -52,8 +53,67 @@ class Vector
             return capacity_;
         }      
 
-        void addElement(const T& element);
-        void removeElement(int index);
+        bool addElement(const T& element)
+        {
+            if(size_ == capacity_){
+                resize();
+            }
+            data_[size_++] = element;
+            return 1;
+        }
+
+
+        bool removeElement(int index);
+        {
+            if(index >= size_)
+            {
+                return 0; //index n'est pas dans le scope du tableau
+            }
+            data_[index] = 0;
+            return 1;
+
+        }
+
+
+        void deleteData()
+        {
+            size_ = 0;
+            capacity_ = 0;
+            delete[] data_;
+        }
+
+        bool VecteurUnused()
+        {
+            if(size == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        bool getData(int index)
+        {
+            if(index >= size_)
+            {
+                return nullptr; //index n'est pas dans le scope du tableau
+            }
+            return data_[Index];
+        }
+
+        void printData (std::ostream& out)
+        {
+            for (int i = 0; i <= size_; ++i) 
+            {
+                out << data[i] << " ";
+            }
+            out << std::endl;
+        }
+
+
+
 };
 
 #endif
